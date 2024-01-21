c9d7a0aa280f34749c61da0eb34cb1a0db0d1b1b
Anudeep Chandra Paul <anudeepch.paul@gmail.com>
2024-01-22 01:59:12 +0530

    Added logger

--- src/app.ts
+++ src/app.ts
@@ -5,10 +5,14 @@ import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone"
 import { readFileSync } from "node:fs";
 import { resolvers } from "./graphql/index.js";
+import { createLogger } from "./utils/Logger.js";
+
+const logger = createLogger('app');
 
 const typeDefs = readFileSync('schema.graphql', 'utf-8');
 
 export async function startServer() {
+  logger.info('Starting server');
   const server = new ApolloServer({
     typeDefs, resolvers
   })
@@ -18,5 +22,6 @@ export async function startServer() {
 }
 
 startServer().then(({ url }) => { 
-  console.log(`Server started at ${url}. Visit ${url}graphql to run queries.`)
+  logger.info(`Server started at ${url}.`)
+  logger.info(`Visit ${url}graphql to run queries.`)
 })
