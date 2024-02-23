155c6da0f1bd3617539825dbd041021353e4e17e
Anudeep Chandra Paul <anudeepch.paul@gmail.com>
2024-02-24 01:39:38 +0530

    eslint fix

--- src/graphql/modules/app/App.resolver.ts
+++ src/graphql/modules/app/App.resolver.ts
@@ -1,17 +1,18 @@
 import { createLoggerByClassName } from "@/utils/Logger";
 
-const logger = createLoggerByClassName('AppResolver');
+const logger = createLoggerByClassName("AppResolver");
+
 export const appResolvers = {
   Query: {
     healthcheck() {
-      logger.debug('Healthcheck query received.');
+      logger.debug("Healthcheck query received.");
       return {
         timenow: new Date().toISOString(),
         serverStatus: "OK",
         dbStatus: "OK",
         nodeEnv: process.env.NODE_ENV || "development",
-        env: process.env.ENV || "development"
-      }
+        env: process.env.ENV || "development",
+      };
     },
   },
-}
+};
--- src/scripts/postBuild.ts
+++ src/scripts/postBuild.ts
@@ -3,7 +3,10 @@ import "tsconfig-paths/register";
 import { copyFile } from "fs";
 import { createLoggerByClassName } from "@/utils/Logger";
 
-const logger = createLoggerByClassName('PostBuild');
+const buildLogger = createLoggerByClassName("PostBuild");
 
-logger.info('Copying schema.graphql file...')
-copyFile(`schema.graphql`, `dist/schema.graphql`, (err) => err ? logger.error(err) : logger.info("Done!"))
+buildLogger.info("Copying schema.graphql file...");
+
+copyFile(`schema.graphql`, `dist/schema.graphql`, (err) =>
+  err ? buildLogger.error(err) : buildLogger.info("Done!"),
+);
